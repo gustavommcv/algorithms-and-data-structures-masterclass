e1e1540e9a767bb95cc43c538627c1aa8d8670ff
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-08-11 20:48:06 -0300

    Bubble sort optimization

--- Sorting/bubbleSort/go/bubble-sort.go
+++ Sorting/bubbleSort/go/bubble-sort.go
@@ -3,11 +3,20 @@ package bubbleSort
 import "cmp"
 
 func BubbleSort[T cmp.Ordered](arr []T) {
+
 	for i := len(arr) - 1; i >= 0; i-- {
+		noSwaps := true
+
 		for j := 0; j < i; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+				noSwaps = false
 			}
 		}
+
+		if noSwaps {
+			break
+		}
 	}
+
 }
